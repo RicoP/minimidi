fa8680dfa8331c77a6b8c241a6a9ab1d927041db
RicoP <RicoP@users.noreply.github.com>
2020-03-12 20:00:14 +0100

    refresh base project

--- premake5.lua
+++ premake5.lua
@@ -5,14 +5,18 @@ workspace "minimidi"
   location ".build/projects"
   targetdir ".build/bin/%{cfg.buildcfg}"
   debugdir "bin"
+  kind "StaticLib"
   language "C++"
+  warnings "Off"
   architecture "x64"
+  cppdialect "C++17"
 
   linkoptions {
+    "/ignore:4006", -- F already defined in X.lib; second definition ignored
   }
 
   filter "configurations:Debug"
-    defines { "DEBUG" }
+    defines { "DEBUG", "EA_DEBUG" }
     symbols "Full"
     optimize "Off"
     targetsuffix "-d"
@@ -22,10 +26,66 @@ workspace "minimidi"
     symbols "Off"
     optimize "Size"
 
+project "_root"
+  kind "None"
+  files { "**" }
+  removefiles { ".git/**" }
+  removefiles { ".build/**" }
+  removefiles { "externals/**" }
+  removefiles { "source/**.cpp", "source/**.h" }
+
 project "app.minimidi"
   kind "ConsoleApp"
   language "C++"
-
-  includedirs { "externals/" }
-
+  warnings "Extra"
+  includedirs { "externals/include" }
+  includedirs { "externals/EABASE/include/Common" }
+  includedirs { "externals/EAAssert/include/" }
+  includedirs { "externals/EASTL/include" }
+  includedirs { "externals/EAThread/include" }
+  includedirs { "externals/EaStdC/include" }
   files { "source/app.minimidi/**.h", "source/app.minimidi/**.c", "source/app.minimidi/**.cpp" }
+  links {  "lib.singleheaders", "lib.eastl", "lib.eaassert", "lib.eastdc" }
+
+project "lib.singleheaders"
+  includedirs { "externals/include" }
+  files { "externals/include/**.h" }
+  files { "source/lib.singleheaders/**.cpp" }
+
+project "lib.eabase"
+  kind "None"
+  files { "externals/EABase/include/Common/**.h" }
+
+project "lib.eaassert"
+  includedirs { "externals/EABASE/include/Common" }
+  includedirs { "externals/EAAssert/include/" }
+  files { "externals/EAAssert/include/**.h" }
+  files { "externals/EAAssert/source/**.cpp" }
+
+project "lib.eastl"
+  includedirs { "externals/EABASE/include/Common" }
+  includedirs { "externals/EAAssert/include/" }
+  includedirs { "externals/EASTL/include" }
+  files { "externals/EASTL/include/**.h" }
+  files { "externals/EASTL/source/**.cpp" }
+  links { "lib.eaassert" }
+
+project "lib.eathread"
+  includedirs { "externals/EABASE/include/Common" }
+  includedirs { "externals/EAAssert/include/" }
+  includedirs { "externals/EASTL/include" }
+  includedirs { "externals/EAThread/include" }
+  files { "externals/EAThread/include/**.h" }
+  files { "externals/EAThread/source/*.cpp" }
+  files { "externals/EAThread/source/pc/*.cpp" }
+  links { "lib.eastl", "lib.eaassert" }
+
+project "lib.eastdc"
+  includedirs { "externals/EABASE/include/Common" }
+  includedirs { "externals/EAAssert/include/" }
+  includedirs { "externals/EASTL/include" }
+  includedirs { "externals/EAThread/include" }
+  includedirs { "externals/EaStdC/include" }
+  files { "externals/EAStdC/include/**.h" }
+  files { "externals/EAStdC/source/**.cpp" }
+  links { "lib.eastl", "lib.eaassert" }
--- source/app.minimidi/main.cpp
+++ source/app.minimidi/main.cpp
@@ -1,50 +1,57 @@
 #include <Windows.h>
+#include <conio.h>  // _getch
+
 #include <iostream>
-#include <conio.h> // _getch
+#include <thread>
 
 #pragma comment(lib, "winmm.lib")
 
 HMIDIOUT omidi;
 HMIDIIN imidi;
 
-void CALLBACK MidiInProc(HMIDIIN hMidiIn, UINT wMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2)
-{
-	if (wMsg == MIM_DATA) {
-		printf("wMsg=MIM_DATA, dwInstance=%08x, dwParam1=%08x, dwParam2=%08x\n", dwInstance, dwParam1, dwParam2);
-		midiOutShortMsg(omidi, dwParam1);
-	}
+void CALLBACK MidiInProc(HMIDIIN hMidiIn, UINT wMsg, DWORD dwInstance, DWORD dwParam1, DWORD dwParam2) {
+  if (wMsg == MIM_DATA) {
+    printf("wMsg=MIM_DATA, dwInstance=%08x, dwParam1=%08x, dwParam2=%08x\n", dwInstance, dwParam1, dwParam2);
+    midiOutShortMsg(omidi, dwParam1);
+  }
 }
 
 int main() {
-	if (midiOutOpen(&omidi, 0, 0, 0, CALLBACK_NULL) == MMSYSERR_NOERROR) {
-		printf("successfully opened default MIDI device \n");
-	}
-	else {
-		printf("error opening default MIDI device \n");
-		return -1;
-	}
+  UINT deviceID = 0;
 
-	if (midiInGetNumDevs() == 0) {
-		fprintf(stderr, "midiInGetNumDevs() return 0...");
-		return -1;
-	}
+  if (midiOutOpen(&omidi, 0, 0, 0, CALLBACK_NULL) == MMSYSERR_NOERROR) {
+    printf("successfully opened default MIDI output device \n");
+  } else {
+    printf("error opening default MIDI device \n");
+    return -1;
+  }
 
-	if (midiInOpen(&imidi, 0, (DWORD_PTR)(void*)MidiInProc, 0, CALLBACK_FUNCTION) != MMSYSERR_NOERROR) {
-		fprintf(stderr, "midiInOpen() failed...");
-		return -1;
-	}
+  if (midiInGetNumDevs() == 0) {
+    fprintf(stderr, "midiInGetNumDevs() return 0...");
+    return -1;
+  }
 
-	midiInStart(imidi);
+  if (midiInOpen(&imidi, deviceID, (DWORD_PTR)(void*)MidiInProc, 0, CALLBACK_FUNCTION) == MMSYSERR_NOERROR) {
+    MIDIINCAPSA dev;
+    if (midiInGetDevCapsA(deviceID, &dev, sizeof(MIDIINCAPS)) == MMSYSERR_NOERROR) {
+      printf("successfully opened MIDI input device '%s' \n", dev.szPname);
+    }
+  } else {
+    fprintf(stderr, "midiInOpen() failed...");
+    return -1;
+  }
 
-	while (_getch() != VK_ESCAPE) {
-	}
+  midiInStart(imidi);
 
-	midiInStop(imidi);
-	midiInClose(imidi);
-	midiOutClose(omidi);
+  while (_getch() != VK_ESCAPE) {
+  }
 
-	imidi = nullptr;
-	omidi = nullptr;
+  midiInStop(imidi);
+  midiInClose(imidi);
+  midiOutClose(omidi);
 
-	return 0;
+  imidi = nullptr;
+  omidi = nullptr;
+
+  return 0;
 }
